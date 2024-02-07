b01d30406f07601a1a9754fc2c42e2c1e93f4798
siaosiaogewun <siaosiaogewun@gmail.com>
2024-02-07 13:20:31 +0800

    添加互转

---
+++ tronaddressbase58tohex.py
@@ -0,0 +1,9 @@
+﻿import base58
+
+base58_address = "TVM9ydprtbdeNeVRxozSfuHry4iRduQc7x"
+
+# Base58 to HEX
+byte_address = base58.b58decode_check(base58_address)
+hex_address = byte_address.hex()
+
+print("Hex Address:", hex_address)
